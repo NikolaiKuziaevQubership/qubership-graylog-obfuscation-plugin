446408df10ac85cbd6dfa8a602293e7b06d2fbb3
IldarMinaev <ildar.minaev.qubership@gmail.com>
2025-03-19 15:39:36 +0400

    chore: format json and js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
-const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
-const path = require('path');
-const buildConfig = loadBuildConfig(path.resolve(__dirname, './build.config'));
+const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig; // eslint-disable-line no-use-before-define
+const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig; // eslint-disable-line no-use-before-define
+const path = require('path'); // eslint-disable-line no-use-before-define
+const buildConfig = loadBuildConfig(path.resolve(__dirname, './build.config')); // eslint-disable-line no-use-before-define
 
 module.exports = new PluginWebpackConfig('org.qubership.graylog2.plugin.ObfuscationPlugin', buildConfig, {
     // Here goes your additional webpack configuration.
